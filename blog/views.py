217b044e83c8845a15af4aa3f5e290103f9e440f
pgrebenkin <pgrebenkin@gmail.com>
2014-11-25 01:29:23 +0200

    Test commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views import generic
+from blog import models
 # Create your views here.
+
+class BlogIndex(generic.ListView):
+	queryset = models.Entry.objects.published()
+	template_name = "home.html"
+	paginate_by = 2
+	
